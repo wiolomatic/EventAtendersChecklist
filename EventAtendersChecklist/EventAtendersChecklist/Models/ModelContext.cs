2d15b82ee329999068f2e0487fb848a443f2c916
rzuraws <rzurawski@pl.sii.eu>
2017-09-15 10:08:58 +0200

    databases creation

---
+++ EventAtendersChecklist/EventAtendersChecklist/Models/ModelContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EventAtendersChecklist.Models
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext()
+            : base("name=ModelContext")
+        { }
+
+        public virtual DbSet<Attender> Attenders { get; set; }
+        public virtual DbSet<Checkbox> Checkboxes { get; set; }
+        public virtual DbSet<Event> Events { get; set; }
+        public virtual DbSet<Header> Headers { get; set; }
+    }
+}
