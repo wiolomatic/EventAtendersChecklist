72ac18c3c8ac30f1f4f45162099b46523843cefb
rzuraws <rzurawski@pl.sii.eu>
2017-09-15 10:08:58 +0200

    databases creation

---
+++ EventAtendersChecklist/EventAtendersChecklist/Models/Checkbox.cs
@@ -0,0 +1,11 @@
+﻿namespace EventAtendersChecklist.Models
+{
+    public class Checkbox
+    {
+        public int Id { get; set; }
+        public string IsChecked { get; set; }
+
+        public virtual Event Event { get; set; }
+        public virtual Attender Attender { get; set; }
+    }
+}
