bd4d8d545318dd26d879fdedeaa85417cda3b7b2
rzuraws <rzurawski@pl.sii.eu>
2017-09-18 13:09:15 +0200

    update

---
+++ EventAtendersChecklist/EventAtendersChecklist/DAL/eacContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using EventAtendersChecklist.Models;
+
+namespace EventAtendersChecklist.DAL
+{
+    public class eacContext : DbContext
+        {
+            public eacContext()
+                : base("DefaultConnection")
+            {
+            }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Models.Action> Actions { get; set; }
+        public DbSet<ActionGroup> ActionGroups { get; set; }
+        public DbSet<EmployeeEventAssignment> EmployeeEventAssignments { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+          //  base.OnModelCreating(modelBuilder);
+        }
+    }
+}
