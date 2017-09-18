8e18166940aa6554b65dbdac11a5380528f2ae9f
rzuraws <rzurawski@pl.sii.eu>
2017-09-18 13:07:00 +0200

    new database implementation

---
+++ EventAtendersChecklist/EventAtendersChecklist/Models/ActionGroup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventAtendersChecklist.Models
+{
+    public class ActionGroup
+    {
+        public ActionGroup()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public int ActionId { get; set; }
+        public int EventId { get; set; }
+        public virtual Action Action { get; set; }
+        public virtual Event Event { get; set; }
+    }
+}
