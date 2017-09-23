fcb065ecf380b17827196efb6c9f0d47054fc7dc
Marcin Pilarczyk <marcinpilarczyk@windowslive.com>
2017-09-23 14:07:56 +0200

    Fix for datatime detail /edit

--- EventAtendersChecklist/EventAtendersChecklist/App_Start/BundleConfig.cs
+++ EventAtendersChecklist/EventAtendersChecklist/App_Start/BundleConfig.cs
@@ -9,8 +9,7 @@ namespace EventAtendersChecklist
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/moment.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -22,12 +21,10 @@ namespace EventAtendersChecklist
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootstrap-datetimepicker.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/bootstrap-datetimepicker.css",
                       "~/Content/site.css"));
         }
     }
