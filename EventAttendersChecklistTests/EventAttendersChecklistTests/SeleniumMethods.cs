e3d16e80aec4a6b3c9e3ee83717fa2458400b97f
Michal Urbanski <murbanski@pl.sii.eu>
2017-09-25 13:36:00 +0200

    added tests solution

---
+++ EventAttendersChecklistTests/EventAttendersChecklistTests/SeleniumMethods.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+
+namespace EventAttendersChecklistTests
+{
+	class SeleniumMethods
+	{
+		public static IWebDriver ConfigureWebDriver(string path)
+		{
+			IWebDriver driver = new InternetExplorerDriver(path);
+			driver.Manage().Window.Maximize();
+
+			return driver;
+		}
+	}
+}
---
+++ EventAttendersChecklistTests/EventAttendersChecklistTests/SeleniumParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace EventAttendersChecklistTests
+{
+	class SeleniumParameters
+	{
+		public static string IeDriverPath = @"../../../Drivers/";
+		public static string WebsiteAddress = "localhost:51722";
+	}
+}
