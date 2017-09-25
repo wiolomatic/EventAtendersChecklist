f9ca9c2fca895383cbdc673d9ee1d0174b899ab8
wiolomatic <wiolomatic@gmail.com>
2017-09-25 13:54:30 +0200

    Role Register

--- EventAtendersChecklist/EventAtendersChecklist/Controllers/AccountController.cs
+++ EventAtendersChecklist/EventAtendersChecklist/Controllers/AccountController.cs
@@ -184,7 +184,7 @@
         /// The Register
         /// </summary>
         /// <returns>The <see cref="ActionResult"/></returns>
-        [AllowAnonymous]
+        [Authorize(Roles = "HR")]
         public ActionResult Register()
         {
             return View();
@@ -198,7 +198,7 @@
         /// <param name="model">The <see cref="RegisterViewModel"/></param>
         /// <returns>The <see cref="Task{ActionResult}"/></returns>
         [HttpPost]
-        [AllowAnonymous]
+        [Authorize(Roles = "HR")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Register(RegisterViewModel model)
         {
